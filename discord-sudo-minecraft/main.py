eb3923eea939db5fd869cc6e5555b51e56d503d5
Arnold Pfahnl <ajpfahnl@gmail.com>
2021-08-25 00:48:43 -0700

    enable logging

--- discord-sudo-minecraft/main.py
+++ discord-sudo-minecraft/main.py
@@ -3,9 +3,20 @@
 from globals import *
 from minecraft_server import MinecraftServerAdmin as MCSA
 from client import SudoMinecraftClient
+import logging
 
 def main():
+    # initiate minecraft server class
     mcsa = MCSA()
+
+    # initiate logging
+    discord_logger = logging.getLogger('discord')
+    discord_logger.setLevel(logging.DEBUG)
+    discord_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    discord_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    discord_logger.addHandler(discord_handler)
+
+    # initiate and run client    
     client = SudoMinecraftClient(mcsa)
     client.run(TOKEN_SUDO_MINECRAFT)
 
